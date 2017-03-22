18fe0b2c5c2e89b158b92381d67a3f545a6a8135
Jhon Alex García Álvarez <Jhon Alex Garca lvarez>
2017-03-22 07:40:55 -0500

    Se crean las las variables

--- ParcialLPAII/src/parciallpaii/ParcialLPAII.java
+++ ParcialLPAII/src/parciallpaii/ParcialLPAII.java
@@ -15,7 +15,9 @@ public class ParcialLPAII {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int horas;
+        int costo_horas;
+        int porcentaje;
     }
     
 }
