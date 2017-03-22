29ef7431de336b3fc0a52544a6d0bf97a4a4c7fc
Jhon Alex García Álvarez <Jhon Alex Garca lvarez>
2017-03-22 07:55:36 -0500

    Se crean comentarios para identificar que hace cada parte del programa

--- ParcialLPAII/src/parciallpaii/ParcialLPAII.java
+++ ParcialLPAII/src/parciallpaii/ParcialLPAII.java
@@ -11,17 +11,18 @@ public class ParcialLPAII {
         int horas;
         int costo_horas;
         int porcentaje;
+        //Variables
         Scanner leer = new Scanner(System.in);
         System.out.println("Ingrese las horas trabajadas");
         horas = leer.nextInt();
         System.out.println("Ingrese el costo por hora");
         costo_horas = leer.nextInt();
         System.out.println("Ingrese el porcentaje de retencion (%)");
-        porcentaje = leer.nextInt();
+        porcentaje = leer.nextInt();//Lecturas
         
         int sueldo_bruto = horas * costo_horas;
         double retencion = sueldo_bruto * (porcentaje /100.0);
-        double sueldo_neto =  sueldo_bruto - retencion;
+        double sueldo_neto =  sueldo_bruto - retencion;//Calculos del programa
     }
     
 }
